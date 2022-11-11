9c4a5e46d79106f87f03104289722cd0f4f94764
Giulia Piombo <giulia.piombo01@gmail.com>
2022-11-11 14:02:43 -0600

    Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import classes from './Home.module.css';
 import me from '../images/me.jpg';
+import background from '../images/background2.jpg';
 
 const Home = () => {
   return (
-    <div className={classes.home}>
+    <div
+      className={classes.home}
+      style={{ backgroundImage: `url(${background})` }}
+    >
       <h1>Hey there, I'm Giulia</h1>
       <img src={me} alt="me" />
       <p>
