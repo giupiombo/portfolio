be32479535b0b89339b505b8610c5f9b6d82abd5
Giulia Piombo <giulia.piombo01@gmail.com>
2022-11-17 19:46:56 -0600

    margins and max-width changes

--- src/components/Desktop/About.js
+++ src/components/Desktop/About.js
@@ -7,37 +7,39 @@ const About = forwardRef((props, ref) => {
       <h1>About Me</h1>
       <div className={classes.row}>
         <div className={classes.column}>
-          <h2>A little bit about myself!</h2>
-          <p>
-            I am originally from <strong>São Paulo, Brazil</strong>. I moved to
-            the United States in 2019 to pursue my bachelor's degree in{' '}
-            <strong>Computer Engineering</strong> at{' '}
-            <strong>Texas Tech University</strong>. I am currently the
-            co-founder and president of <strong>BRASA at Tech</strong>, a
-            Brazilian student organization at the university. Our goal is to
-            connect Brazilian students, promote our culture, and support
-            volunteering activities to help our local community.
-          </p>
-          <p>
-            Proactive <strong>Software Engineer</strong> passionate about
-            applying my academic knowledge in a practical and efficient way to
-            develop purposeful software. Eager to learn new industry best
-            practices to improve software development.
-          </p>
-          <h3 className={classes.facts}>Fun facts</h3>
-          <li>
-            I have a 1 year old cat called Stella. I adopted her when she was 5
-            weeks old at the shelter.
-          </li>
-          <li>
-            When I was younger I used to draw and paint. I still like to do it
-            when I have some spare time.
-          </li>
-          <li>
-            Sports have always been part of my life. Every since I was young I
-            used to swim and play soccer. Nowadays I like hiking, boxing and
-            muay-thai.
-          </li>
+          <div className={classes.margin}>
+            <h2>A little bit about myself!</h2>
+            <p>
+              I am originally from <strong>São Paulo, Brazil</strong>. I moved
+              to the United States in 2019 to pursue my bachelor's degree in{' '}
+              <strong>Computer Engineering</strong> at{' '}
+              <strong>Texas Tech University</strong>. I am currently the
+              co-founder and president of <strong>BRASA at Tech</strong>, a
+              Brazilian student organization at the university. Our goal is to
+              connect Brazilian students, promote our culture, and support
+              volunteering activities to help our local community.
+            </p>
+            <p>
+              Proactive <strong>Software Engineer</strong> passionate about
+              applying my academic knowledge in a practical and efficient way to
+              develop purposeful software. Eager to learn new industry best
+              practices to improve software development.
+            </p>
+            <h3 className={classes.facts}>Fun facts</h3>
+            <li>
+              I have a 1 year old cat called Stella. I adopted her when she was
+              5 weeks old at the shelter.
+            </li>
+            <li>
+              When I was younger I used to draw and paint. I still like to do it
+              when I have some spare time.
+            </li>
+            <li>
+              Sports have always been part of my life. Every since I was young I
+              used to swim and play soccer. Nowadays I like hiking, boxing and
+              muay-thai.
+            </li>
+          </div>
         </div>
         <div className={classes.column}>
           <h2>Technical Skills</h2>
