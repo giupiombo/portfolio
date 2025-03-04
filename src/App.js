40da9f3831ea7960a5887234f124b694024f0999
Giulia Piombo <giupiombo@Giulias-MBP.attlocal.net>
2025-03-03 18:14:55 -0600

    fix: redirect to new portfolio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { useMediaQuery } from 'react-responsive';
 import DesktopView from './components/Desktop/DesktopView';
 import MobileView from './components/Mobile/MobileView';
+import { useEffect } from 'react';
 
 function App() {
   const isMobile = useMediaQuery({
     query: '(max-width: 1000px)',
   });
+
+  useEffect(() => {
+    window.location.href = 'https://giupiombo-portfolio.vercel.app'; // Change to your new portfolio URL
+  }, []);
 
   return <>{isMobile ? <MobileView /> : <DesktopView />}</>;
 }
